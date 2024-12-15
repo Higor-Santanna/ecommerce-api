b7267ff30c09272c64d0e5266fb983ad0f08a2ab
Higor Santanna Rodrigues <higorrodrigues44@outlook.com>
2024-12-15 15:34:19 -0300

    adicioanado a pasta erros e a pasta middleware para o tratamento de erros. Erro de servidor e de email já tratado

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { getFirestore } from "firebase-admin/firestore";
+import { ValidationError } from "../errors/validation.error";
 
 type User = {
     id: number,
@@ -23,22 +24,23 @@ export class UsersController {
         };
     };
 
-    static async save(req: Request, res: Response) {
+    static async save(req: Request, res: Response, next: NextFunction) {
         try {
             let user = req.body;
+            if (!user.email || user.email?.length === 0){
+                throw new ValidationError("E-mail obrigatório")
+            };
             const userSave = await getFirestore().collection("users").add(user); //Nesse ponto ele cria uma coleção e faz a conexão deste controller com nosso banco dados onde será adicionado novos usuários
             res.status(201).send({
                 message: `Usuário ${userSave} criado com sucesso.`
             });
         } catch (error) {
-            res.status(500).send({
-                message: "Erro interno do Servidor"
-            });
+            next(error)
         };
 
     };
 
-    static async getById(req: Request, res: Response) {
+    static async getById(req: Request, res: Response, next: NextFunction) {
         try {
             let userId = req.params.id; //pega o e armazena o ID do usuário
             const doc = await getFirestore().collection("users").doc(userId).get(); //Trás da coleção o ID especifico solicitado pelo usuário
@@ -47,13 +49,11 @@ export class UsersController {
                 ...doc.data()
             });
         } catch (error) {
-            res.status(500).send({
-                message: "Erro interno do Servidor"
-            });
+           next(error);
         };
-    }
+    };
 
-    static async update(req: Request, res: Response) {
+    static async update(req: Request, res: Response, next: NextFunction) {
         try {
             let userId = req.params.id;
             let user = req.body as User;
@@ -63,21 +63,17 @@ export class UsersController {
             });
             res.send({ message: "Informações do usuário atualizado" });
         } catch (error) {
-            res.status(500).send({
-                message: "Erro interno do Servidor"
-            });
+            next(error);
         };
-    }
+    };
 
-    static async delete(req: Request, res: Response) {
+    static async delete(req: Request, res: Response, next: NextFunction) {
         try {
             let userId = req.params.id;
             await getFirestore().collection("users").doc(userId).delete();
             res.send({ message: "Usuário deletado" });
         } catch (error) {
-            res.status(500).send({
-                message: "Erro interno do Servidor"
-            });
-        }
-    }
+            next(error);
+        };
+    };
 };
