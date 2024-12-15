ac8db3d1b4b53b3a16b58bafb8b3501a16958fa3
Higor Santanna Rodrigues <higorrodrigues44@outlook.com>
2024-12-15 11:05:04 -0300

    Adicionado o arquivo middleware com o tratamento de erros

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from "express";
-import {getFirestore} from "firebase-admin/firestore";
+import { NextFunction, Request, Response } from "express";
+import { getFirestore } from "firebase-admin/firestore";
 
 type User = {
     id: number,
@@ -8,47 +8,76 @@ type User = {
 }
 
 export class UsersController {
-    static async getAll (req: Request, res: Response){
-        const snapshot = await getFirestore().collection("users").get(); //nesse ponto ele conecta com o banco de dados e trás todas as informações que está neste endpoint
-        const users = snapshot.docs.map(doc => {
-            return {
-                id: doc.id,
-                ...doc.data()
-            };
-        });
-        res.json(users) //Essa função do Express automaticamente converte o objeto JavaScript users em uma string JSON e a envia como resposta.
+    static async getAll(req: Request, res: Response, next: NextFunction) {
+        try {
+            const snapshot = await getFirestore().collection("users").get(); //nesse ponto ele conecta com o banco de dados e trás todas as informações que está neste endpoint
+            const users = snapshot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data()
+                };
+            });
+            res.json(users) //Essa função do Express automaticamente converte o objeto JavaScript users em uma string JSON e a envia como resposta.
+        } catch (error) {
+            next(error);
+        };
     };
 
-    static async save(req: Request, res: Response){
-        let user = req.body;
-        const userSave = await getFirestore().collection("users").add(user); //Nesse ponto ele cria uma coleção e faz a conexão deste controller com nosso banco dados onde será adicionado novos usuários
-        res.send({
-            message: `Usuário ${userSave} criado com sucesso.`
-        });
+    static async save(req: Request, res: Response) {
+        try {
+            let user = req.body;
+            const userSave = await getFirestore().collection("users").add(user); //Nesse ponto ele cria uma coleção e faz a conexão deste controller com nosso banco dados onde será adicionado novos usuários
+            res.status(201).send({
+                message: `Usuário ${userSave} criado com sucesso.`
+            });
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro interno do Servidor"
+            });
+        };
+
     };
 
-    static async getById(req: Request, res: Response){
-        let userId = req.params.id; //pega o e armazena o ID do usuário
-        const doc = await getFirestore().collection("users").doc(userId).get(); //Trás da coleção o ID especifico solicitado pelo usuário
-        res.send({
+    static async getById(req: Request, res: Response) {
+        try {
+            let userId = req.params.id; //pega o e armazena o ID do usuário
+            const doc = await getFirestore().collection("users").doc(userId).get(); //Trás da coleção o ID especifico solicitado pelo usuário
+            res.send({
                 id: doc.id,
                 ...doc.data()
             });
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro interno do Servidor"
+            });
+        };
     }
 
-    static async update(req: Request, res: Response){
-        let userId = req.params.id;
-        let user = req.body as User;
-        await getFirestore().collection("users").doc(userId).set({
-            nome: user.nome,
-            email : user.email
-        });
-        res.send({ message: "Informações do usuário atualizado"});
+    static async update(req: Request, res: Response) {
+        try {
+            let userId = req.params.id;
+            let user = req.body as User;
+            await getFirestore().collection("users").doc(userId).set({
+                nome: user.nome,
+                email: user.email
+            });
+            res.send({ message: "Informações do usuário atualizado" });
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro interno do Servidor"
+            });
+        };
     }
 
-    static async delete(req: Request, res: Response){
-        let userId = req.params.id;
-        await getFirestore().collection("users").doc(userId).delete();
-        res.send({ message: "Usuário deletado"});
+    static async delete(req: Request, res: Response) {
+        try {
+            let userId = req.params.id;
+            await getFirestore().collection("users").doc(userId).delete();
+            res.send({ message: "Usuário deletado" });
+        } catch (error) {
+            res.status(500).send({
+                message: "Erro interno do Servidor"
+            });
+        }
     }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { initializeApp } from 'firebase-admin/app';
 import { routes } from "./routes";
+import { errorHandler } from "./middlewares/error-handler.middleware";
 
 const app = express();
 const port = 3000;
 
 initializeApp();
 routes(app);
+errorHandler(app)
 
 app.listen(port, () => {
     console.log(`Servidor funcionando na porta ${port}`);
