e56b0f6d018b6520fcbcc3bbca9dba691175788b
Higor Santanna Rodrigues <higorrodrigues44@outlook.com>
2024-12-15 17:38:51 -0300

    Módulo tratamento de erros concluido

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -11,80 +11,59 @@ type User = {
 
 export class UsersController {
     static async getAll(req: Request, res: Response, next: NextFunction) {
-        try {
-            const snapshot = await getFirestore().collection("users").get(); //nesse ponto ele conecta com o banco de dados e trás todas as informações que está neste endpoint
-            const users = snapshot.docs.map(doc => {
-                return {
-                    id: doc.id,
-                    ...doc.data()
-                };
-            });
-            res.json(users) //Essa função do Express automaticamente converte o objeto JavaScript users em uma string JSON e a envia como resposta.
-        } catch (error) {
-            next(error);
-        };
+        const snapshot = await getFirestore().collection("users").get(); //nesse ponto ele conecta com o banco de dados e trás todas as informações que está neste endpoint
+        const users = snapshot.docs.map(doc => {
+            return {
+                id: doc.id,
+                ...doc.data()
+            };
+        });
+        res.json(users) //Essa função do Express automaticamente converte o objeto JavaScript users em uma string JSON e a envia como resposta.
     };
 
     static async save(req: Request, res: Response, next: NextFunction) {
-        try {
-            let user = req.body;
-            if (!user.email || user.email?.length === 0){
-                throw new ValidationError("E-mail obrigatório")
-            };
-            const userSave = await getFirestore().collection("users").add(user); //Nesse ponto ele cria uma coleção e faz a conexão deste controller com nosso banco dados onde será adicionado novos usuários
-            res.status(201).send({
-                message: `Usuário ${userSave} criado com sucesso.`
-            });
-        } catch (error) {
-            next(error)
+        let user = req.body;
+        if (!user.email || user.email?.length === 0) {
+            throw new ValidationError("E-mail obrigatório")
         };
-
+        const userSave = await getFirestore().collection("users").add(user); //Nesse ponto ele cria uma coleção e faz a conexão deste controller com nosso banco dados onde será adicionado novos usuários
+        res.status(201).send({
+            message: `Usuário ${userSave} criado com sucesso.`
+        });
     };
 
     static async getById(req: Request, res: Response, next: NextFunction) {
-        try {
-            let userId = req.params.id; //pega o e armazena o ID do usuário
-            const doc = await getFirestore().collection("users").doc(userId).get(); //Trás da coleção o ID especifico solicitado pelo usuário
-            if (doc.exists) { //doc.exists é uma propriedade do documento que confirma sua existância.
-                res.send({
-                    id: doc.id,
-                    ...doc.data()
-                });   
-            } else {
-                throw new NotFoundError("Usuário não encontrado")
-            };
-        } catch (error) {
-           next(error);
+        let userId = req.params.id; //pega o e armazena o ID do usuário
+        const doc = await getFirestore().collection("users").doc(userId).get(); //Trás da coleção o ID especifico solicitado pelo usuário
+        if (doc.exists) { //doc.exists é uma propriedade do documento que confirma sua existância.
+            res.send({
+                id: doc.id,
+                ...doc.data()
+            });
+        } else {
+            throw new NotFoundError("Usuário não encontrado")
         };
     };
 
     static async update(req: Request, res: Response, next: NextFunction) {
-        try {
-            let userId = req.params.id;
-            let user = req.body as User;
-            let docRef = getFirestore().collection("users").doc(userId);
+        let userId = req.params.id;
+        let user = req.body as User;
+        let docRef = getFirestore().collection("users").doc(userId);
 
-            if((await docRef.get()).exists){
-                await docRef.set({
-                    nome: user.nome,
-                    email: user.email
-                });
-                res.status(201).send({ message: "Informações do usuário atualizado" });
-            } else {
-                throw new NotFoundError("Usuário não encontrado")
-            }
-        } catch (error) {
-            next(error);
-        };
+        if ((await docRef.get()).exists) {
+            await docRef.set({
+                nome: user.nome,
+                email: user.email
+            });
+            res.status(201).send({ message: "Informações do usuário atualizado" });
+        } else {
+            throw new NotFoundError("Usuário não encontrado")
+        }
     };
 
     static async delete(req: Request, res: Response, next: NextFunction) {
-        try {
-            let userId = req.params.id;
-            await getFirestore().collection("users").doc(userId).delete();
-            res.status(204).send({ message: "Usuário deletado" });
-        } catch (error) {
-            next(error);
-        };
+        let userId = req.params.id;
+        await getFirestore().collection("users").doc(userId).delete();
+        res.status(204).send({ message: "Usuário deletado" });
     };
 };
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { initializeApp } from 'firebase-admin/app';
 import { routes } from "./routes";
 import { errorHandler } from "./middlewares/error-handler.middleware";
+import { pageNotFoundHandler } from "./middlewares/page-not-found.middleware";
 
 const app = express();
 const port = 3000;
 
 initializeApp();
 routes(app);
+pageNotFoundHandler(app)
 errorHandler(app)
 
 app.listen(port, () => {
